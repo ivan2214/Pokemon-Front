7e43cef3db374b4bbec71aa333308e8ef24f42f2
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-11-21 21:49:54 -0300

    adind responsive

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -6,26 +6,23 @@ import img from "../../assets/gifts/charmander.gif";
 const Landing = () => {
   return (
     <main className="landingPage">
-      <section className="sec-texts">
-        <div className="sec-textos">
-          <div className="sec-pg">
-            <h1>¡Bienvenido a tu pokedex!</h1>
-            <p>
-              Aquí puedes encontrar toda la info de tus pokemons favoritos.
-              ¡También puedes dejar volar tu creatividad y crear un nuevo
-              Pokémon exclusivo! Espero que te guste :)
-            </p>
-          </div>
-          <Link to="/home" className="btn">
-            GO Home !
-          </Link>
+      <section className="sec-textos">
+        <div className="sec-pg">
+          <h1>¡Bienvenido a tu pokedex!</h1>
+          <p>
+            Aquí puedes encontrar toda la info de tus pokemons favoritos.
+            ¡También puedes dejar volar tu creatividad y crear un nuevo Pokémon
+            exclusivo! Espero que te guste :)
+          </p>
         </div>
+        <Link to="/home" className="btn">
+          GO Home !
+        </Link>
       </section>
-      <section className="sec-img">
-        <picture>
-          <img loading="lazi" src={img} alt="" />
-        </picture>
-      </section>
+
+      <figure className="sec-img">
+        <img loading="lazi" src={img} alt="" />
+      </figure>
     </main>
   );
 };
