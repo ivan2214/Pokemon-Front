2d0f2e64b263f49a4ffd3f6b4773fedce01c006d
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-11-23 22:41:34 -0300

    fixind keys

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-/* axios.defaults.baseURL = "https://pokemon-back-production.up.railway.app/"; */
+/* axios.defaults.baseURL = "http://localhost:3001"; */
+axios.defaults.baseURL = "https://pokemon-back-production.up.railway.app/";
 
 ReactDOM.render(
   <React.StrictMode>
